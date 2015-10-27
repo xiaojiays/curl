d026888449f487edeaab522f5de6adbeb927fa8d
mm <mm@localhost.localdomain>
2015-10-27 04:08:05 -0700

    add in time functions

--- src/Xiaojiays/Curl/CurlService.php
+++ src/Xiaojiays/Curl/CurlService.php
@@ -18,4 +18,42 @@ class CurlService
         $curl->setOption(CURLOPT_POSTFIELDS, $postData);
         return $curl->send();
     }
+
+    public function getInSeconds($url, $seconds)
+    {
+        $curl = new Curl;
+        $curl->setOption(CURLOPT_URL, $url);
+        $curl->setOption(CURLOPT_TIMEOUT, $seconds);
+        return $curl->send();
+    }
+
+    public function postInSeconds($url, $postData, $seconds)
+    {
+        $curl = new Curl;
+        $curl->setOption(CURLOPT_URL, $url);
+        $curl->setOption(CURLOPT_POST, 1);
+        $curl->setOption(CURLOPT_POSTFIELDS, $postData);
+        $curl->setOption(CURLOPT_TIMEOUT, $seconds);
+        return $curl->send();
+    }
+
+    public function getInMilliseconds($url, $milliseconds)
+    {
+        $curl = new Curl;
+        $curl->setOption(CURLOPT_URL, $url);
+        $curl->setOption(CURLOPT_NOSIGNAL, true);
+        $curl->setOption(CURLOPT_TIMEOUT_MS, $milliseconds);
+        return $curl->send();
+    }
+
+    public function postInMilliseconds($url, $postData, $milliseconds)
+    {
+        $curl = new Curl;
+        $curl->setOption(CURLOPT_URL, $url);
+        $curl->setOption(CURLOPT_POST, 1);
+        $curl->setOption(CURLOPT_POSTFIELDS, $postData);
+        $curl->setOption(CURLOPT_NOSIGNAL, true);
+        $curl->setOption(CURLOPT_TIMEOUT_MS, $milliseconds);
+        return $curl->send();
+    }
 }
