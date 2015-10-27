38685f1b284517289f11a21f13b896722d7dd430
mm <mm@localhost.localdomain>
2015-10-27 03:30:11 -0700

    add post

--- src/Xiaojiays/Curl/CurlService.php
+++ src/Xiaojiays/Curl/CurlService.php
@@ -1,12 +1,21 @@
 <?php namespace Xiaojiays\Curl;
 
-class CurlService extends Base
+class CurlService
 {
   
     public function get($url)
     {
         $curl = new Curl;
         $curl->setOption(CURLOPT_URL, $url);
+        return $curl->send();
+    }
+
+    public function post($url, $postData)
+    {
+        $curl = new Curl;
+        $curl->setOption(CURLOPT_URL, $url);
+        $curl->setOption(CURLOPT_POST, 1);
+        $curl->setOption(CURLOPT_POSTFIELDS, $postData);
         return $curl->send();
     }
 }
